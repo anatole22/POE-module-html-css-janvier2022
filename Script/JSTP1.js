f5f3a225f680ce150404a81f31707eafcda41fdf
anatole22 <anatole.viaud@gmail.com>
2022-01-20 10:16:58 +0100

    exercice TP1JS

---
+++ Script/JSTP1.js
@@ -0,0 +1,6 @@
+let number1 = 15;
+let number2 = 100;
+const resultat = number1 + number2;
+alert("Bienvenu sur la page de formulaire");
+console.log(number1, number2);
+console.log("Le résultat est" + " " + resultat);
