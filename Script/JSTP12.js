c7afffe75e630e254dce0c19c7be64a31feef216
anatole22 <anatole.viaud@gmail.com>
2022-01-24 15:10:49 +0100

    Ajout tpjs 12 & 13

---
+++ Script/JSTP12.js
@@ -0,0 +1,16 @@
+const array = [];
+let res = 0;
+function pushArray() {
+  let push = document.getElementById("array").value;
+  array.push(push);
+  console.log(array);
+}
+function init() {
+  location.reload();
+}
+function calcul() {
+  for (let i = 0; i < array.length; i++) {
+    res = res + parseInt(array[i]);
+    console.log(res);
+  }
+}
