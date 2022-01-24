639e1fedbe06e649d541bf3beda60e2ab795abec
anatole22 <anatole.viaud@gmail.com>
2022-01-24 19:07:07 +0100

    Ajout TPJS17

---
+++ Script/JSTP17.js
@@ -0,0 +1,20 @@
+const tailleMatrice = prompt("Entrer la taille de la matrice");
+let array = [];
+let matrice = [];
+let diagonale = 0;
+let diagonale2 = 0;
+for (let i = 0; i < tailleMatrice; i++) {
+  array = [];
+  for (let j = 0; j < tailleMatrice; j++) {
+    array.push(prompt("définir l'emplacement" + " " + i + " " + j));
+    if (i === j) {
+      diagonale += parseInt(array[j]);
+    }
+    if (j === tailleMatrice - (1 + i)) {
+      diagonale2 += parseInt(array[j]);
+    }
+  }
+  matrice.push(array);
+}
+console.log(diagonale);
+console.log(diagonale2);
